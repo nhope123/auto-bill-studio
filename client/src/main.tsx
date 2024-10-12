ff79b23ddc9e8cf1a6bdc56602cdd5584120e7c1
Nial Hope <nyevincy@gmail.com>
2024-10-12 03:54:12 -0400

    initialize app settings

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
